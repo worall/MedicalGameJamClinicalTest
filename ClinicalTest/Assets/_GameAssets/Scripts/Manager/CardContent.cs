1b05649d4362c149f73af62187c8b24e26458d1b
Mikaël <lorack@gmail.com>
2019-11-16 16:11:55 +0100

    Add basic DeckManager

---
+++ ClinicalTest/Assets/_GameAssets/Scripts/Manager/CardContent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardContent
+{
+    public int id;
+
+    public string name;
+    public string situation;
+
+    public string choiceLeft;
+    public string choiceRight;
+
+    public string resLeft;
+    public string resRight;
+
+    public string time;
+    public string turns;
+}
--- ClinicalTest/Assets/_GameAssets/Scripts/Manager/DeckManager.cs
+++ ClinicalTest/Assets/_GameAssets/Scripts/Manager/DeckManager.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class DeckManager : MonoBehaviour
 {
+    List<CardContent> cards = new List<CardContent>();
+    List<List<CardContent>> pools = new List<List<CardContent>>();
+
+    private char lineSeperater = '\n'; // It defines line seperate character
+    private char fieldSeperator = ';'; // It defines field seperate chracter
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      this.parseResource();
+      this.initPools();
+
+      // DEBUG
+      CardContent next = this.draw(1);
+      Debug.Log(next.name);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    CardContent draw(int turn) {
+      int nbCards = this.pools[turn].Count;
+      int selected = Random.Range(0, nbCards);
+      CardContent nextCard = this.pools[turn][selected];
+
+      return nextCard;
+    }
+
+    private void parseResource() {
+      TextAsset rawContent = Resources.Load<TextAsset>("cards");
+
+      string[] records = rawContent.text.Split(lineSeperater);
+
+      foreach (string record in records) {
+        string[] row = record.Split(fieldSeperator);
+
+        CardContent card = new CardContent();
+        card.name = row[0];
+        card.situation = row[1];
+        card.choiceLeft = row[2];
+        card.choiceRight = row[3];
+        card.resLeft = row[4];
+        card.resRight = row[5];
+        card.time = row[14];
+        card.turns = row[15];
+
+        cards.Add(card);
+      }
+    }
+
+    private void initPools() {
+      // Init empty pools
+      for (int i = 0; i < 20; i++) {
+        this.pools.Add(new List<CardContent>());
+      }
+
+      // Populate pools
+      foreach (CardContent card in this.cards) {
+        string[] turns = card.turns.Split(':');
+        foreach (string rawTurn in turns) {
+          int turn;
+          int.TryParse(rawTurn, out turn);
+          this.pools[turn].Add(card);
+        }
+      }
+    }
+
+    private void updatePools() {
+      // Modify pools if need be
     }
 }
