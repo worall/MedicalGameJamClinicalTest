89a2392cf4e4e2abb44f2f2a118a8abd3f0282bc
Allan <a.boulogne@student.isartdigital.com>
2019-11-16 17:18:41 +0100

    good prefab

--- ClinicalTest/Assets/_GameAssets/Scripts/DummyLogic.cs
+++ ClinicalTest/Assets/_GameAssets/Scripts/DummyLogic.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class DummyLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        CardBehaviour card = GameObject.Find("CardRoot").GetComponent<CardBehaviour>();
-        card.onSwipeNo = dummySwipeY;
-        card.onSwipeYes = dummySwipeN;
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+    CardBehaviour card = GameObject.Find("CardRoot").GetComponent<CardBehaviour>();
+    card.onSwipeNo = dummySwipeY;
+    card.onSwipeYes = dummySwipeN;
+  }
 
-    void dummySwipeY(CardBehaviour.CardEffects effects)
-    {
-        print("YES!!");
-    }
-    void dummySwipeN(CardBehaviour.CardEffects effects)
-    {
-        print("NO");
-    }
+  void dummySwipeY(CardBehaviour.CardEffects effects)
+  {
+    print("YES!!");
+  }
+  void dummySwipeN(CardBehaviour.CardEffects effects)
+  {
+    print("NO");
+  }
 }
