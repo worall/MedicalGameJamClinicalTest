fea4b2adf4911e0bc7b6551c62a9f3ecfb8199fb
Olivier Guyot <olivier.guyot@camptocamp.com>
2019-12-01 15:42:28 +0100

    Added a button manager
    The home button does nothing for now.
    The help button summons a card overlay with the tuto card.

---
+++ ClinicalTest/Assets/_GameAssets/Scripts/Manager/ButtonManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] public Button homeButton;
+    [SerializeField] public Button helpButton;
+
+    [SerializeField] public OverlayCardContainerBehaviour overlayCardPrefab;
+    [SerializeField] public CardBehaviour helpCard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        homeButton.onClick.AddListener(OnClickHome);
+        helpButton.onClick.AddListener(OnClickHelp);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnClickHelp() {
+        OverlayCardContainerBehaviour overlay = Instantiate(overlayCardPrefab);
+        overlay.cardPrefab = helpCard;
+    }
+
+    void OnClickHome() {
+        Debug.Log("Home clicked");
+    }
+}
