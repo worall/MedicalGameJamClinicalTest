1f175f6d012b5ff2d3fadc650b36c05d812315b7
Olivier Guyot <olivier.guyot@camptocamp.com>
2019-12-07 19:31:32 +0100

    add missing script

---
+++ ClinicalTest/Assets/CameraShakeBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShakeBehaviour : MonoBehaviour
+{
+    float shakeRatio = 0;
+    Vector3 originalPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shakeRatio > 0.001) {
+            Vector2 offset = Random.insideUnitCircle * 24 * shakeRatio;
+            this.transform.position = new Vector3(
+                originalPos.x + offset.x * 2,
+                originalPos.y + offset.y,
+                originalPos.z);
+            shakeRatio *= 0.75f;
+        } else {
+            this.transform.position = originalPos;
+        }
+
+    }
+
+    public void Shake() {
+        shakeRatio = 1;
+    }
+}
