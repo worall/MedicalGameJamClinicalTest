3f5da5ba8c377799170b9f33c10da534156c51e2
Mikaël <lorack@gmail.com>
2019-12-11 00:25:49 +0100

    Create new content structure for Popup Card

---
+++ ClinicalTest/Assets/_GameAssets/Scripts/DataStructure/PopupCardContent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupCardContent
+{
+    public int id;
+    public string name;
+
+    public string title;
+    public string description;
+    public string options;
+}
