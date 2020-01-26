25c1810742ed27955b79376bf23440418a6f90fe
jahow <jahow_dude@yahoo.fr>
2020-01-26 15:13:25 +0100

    fix compare link card behaviour (onswipeyes was not correct)

--- ClinicalTest/Assets/_GameAssets/Scripts/CompareLinkCardBehaviour.cs
+++ ClinicalTest/Assets/_GameAssets/Scripts/CompareLinkCardBehaviour.cs
@@ -6,10 +6,14 @@ public class CompareLinkCardBehaviour : MonoBehaviour
 {
     void Awake() {
         CardBehaviour behaviour = this.GetComponent<CardBehaviour>();
-        behaviour.onSwipeYes = HandleCardSwipe;
         behaviour.cardContent = new CardContent();
         behaviour.cardContent.yes.choice = "Ouvrir le site internet de COMPARE";
         behaviour.cardContent.no.choice = "Continuer le jeu";
+    }
+
+    void Start() {
+        CardBehaviour behaviour = this.GetComponent<CardBehaviour>();
+        behaviour.onSwipeYes = HandleCardSwipe;
     }
 
     bool HandleCardSwipe(CardEffect effect, bool swipedRight) {
